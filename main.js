307a55081d6523b62080e2419b28dd8b0bba7a42
assettasimone <assettasimone@gmail.com>
2025-07-24 18:42:03 +0200

    add file link in html document

---
+++ main.js
@@ -0,0 +1,17 @@
+//recupero i campi del form
+const tripKmField = document.getElementById("trip-km");
+const passengerAgeField = document.getElementById("passenger-age");
+
+//aspetto che il pulsante viene premuto
+button.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const tripKm = tripKmField.value
+    const passengerAge = passengerAgeField.value
+    console.log(tripKm);
+    console.log(passengerAge);
+
+
+
+
+})
