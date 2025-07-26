fe1b867094309cd08762053f59c251f1c3bacc3e
assettasimone <assettasimone@gmail.com>
2025-07-26 18:07:36 +0200

    add km price variable

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const passengerAgeField = document.getElementById("passenger-age");
 //pulsante
 const fomrEl = document.querySelector("form")
 
+Const priceKm = 0.21;
+
 //recupero il campo disabilitato per modificarlo dinamicamente all'inserimento dell'età
 const passengerTypeField = document.getElementById("passenger-type")
 console.log(passengerTypeField);
@@ -34,16 +36,16 @@ fomrEl.addEventListener('submit', (e) => {
     e.preventDefault();
 
     //recupero i dati dal form
-    const tripKm = tripKmField.value
-    const passengerAge = passengerAgeField.value
+    const tripKm = number(tripKmField.value)
+    const passengerAge = number(passengerAgeField.value)
     let ticketPrice = 0;
     //calcoliamo il prezzo in base all'età
     if (passengerAge < 18) {
-        ticketPrice = (parseInt(tripKm) * 0.21 * 0.8).toFixed(2);
+        ticketPrice = (tripKm * priceKm * 0.8).toFixed(2);
     } else if (passengerAge > 65) {
-        ticketPrice = (parseInt(tripKm) * 0.21 * 0.6).toFixed(2);
+        ticketPrice = (tripKm * priceKm * 0.6).toFixed(2);
     } else {
-        ticketPrice = (parseInt(tripKm) * 0.21).toFixed(2);
+        ticketPrice = (tripKm * priceKm).toFixed(2);
     }
 
     console.log(ticketPrice);
